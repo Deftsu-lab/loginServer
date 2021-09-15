4effa41b8c72c633c3f074a5b17f5b403240662c
BenderDähnung <benlucabehring@gmail.com>
2021-09-16 01:11:38 +0200

    make it better

---
+++ config/db.js
@@ -0,0 +1,12 @@
+require('dotenv').config({ path: 'C:/Users/benlu/Desktop/Plugged/login_server/.env' });
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((err) => console.log(err));
