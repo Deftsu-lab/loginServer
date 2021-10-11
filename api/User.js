dd605ba052805891a8413a518b1c4ab2ebac9494
BenderDähnung <benlucabehring@gmail.com>
2021-10-11 15:42:00 +0200

    heroku update

--- api/User.js
+++ api/User.js
@@ -1,35 +1,57 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // mongodb user model
-const User = require('./../models/User');
+const User = require("./../models/User");
 
 // mongodb user verification model
-const UserVerification = require('./../models/UserVerification');
+const UserVerification = require("./../models/UserVerification");
+
+// mongodb password reset model
+const PasswordReset = require("./../models/PasswordReset");
 
 // Password handler
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
-//email handler
-const nodemailer = require('nodemailer');
+//email stuff
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 
 //unique string
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 //env variables
-require('dotenv').config({
-	path: 'C:/Users/benlu/Desktop/Plugged/login_server/.env',
+require("dotenv").config({
+	path: "C:/Users/benlu/Desktop/Plugged/login_server/.env",
 });
 
 //path for static verified page
-const path = require('path');
+const path = require("path");
+const { error } = require("console");
 
 //nodemailer stuff
+
+const oauth2Client = new OAuth2(
+	process.env.GOOGLE_CLIENT_ID, //google client id
+	process.env.GOOGLE_SECRET, //google secret
+	"https://developers.google.com/oauthplayground" //redirect url
+);
+
+oauth2Client.setCredentials({
+	refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+});
+const accessToken = oauth2Client.getAccessToken();
+
 let transporter = nodemailer.createTransport({
-	service: 'gmail',
+	service: "gmail",
 	auth: {
+		type: "OAuth2",
 		user: process.env.AUTH_EMAIL,
-		pass: process.env.AUTH_PASS,
+		clientId: process.env.GOOGLE_CLIENT_ID,
+		clientSecret: process.env.GOOGLE_SECRET,
+		refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+		accessToken: accessToken
 	},
 });
 
@@ -38,34 +60,34 @@ transporter.verify((error, success) => {
 	if (error) {
 		console.log(error);
 	} else {
-		console.log('Ready for message');
+		console.log("Ready for message");
 		console.log(success);
 	}
 });
 
 // Signup
-router.post('/signup', (req, res) => {
+router.post("/signup", (req, res) => {
 	let { name, email, password, dateOfBirth } = req.body;
 
-	if (name == '' || email == '' || password == '' || dateOfBirth == '') {
+	if (name == "" || email == "" || password == "" || dateOfBirth == "") {
 		res.json({
-			status: 'FAILED',
-			message: 'Empty input fields!',
+			status: "FAILED",
+			message: "Empty input fields!",
 		});
 	} else if (!/^[a-zA-Z ]*$/.test(name)) {
 		res.json({
-			status: 'FAILED',
-			message: 'Invalid name entered',
+			status: "FAILED",
+			message: "Invalid name entered",
 		});
 	} else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
 		res.json({
-			status: 'FAILED',
-			message: 'Invalid email entered',
+			status: "FAILED",
+			message: "Invalid email entered",
 		});
 	} else if (password.length < 8) {
 		res.json({
-			status: 'FAILED',
-			message: 'Password is too short!',
+			status: "FAILED",
+			message: "Password is too short!",
 		});
 	} else {
 		// Checking if user already exists
@@ -74,8 +96,8 @@ router.post('/signup', (req, res) => {
 				if (result.length) {
 					// A user already exists
 					res.json({
-						status: 'FAILED',
-						message: 'User with the provided email already exists',
+						status: "FAILED",
+						message: "User with the provided email already exists",
 					});
 				} else {
 					// Try to create new user
@@ -100,15 +122,15 @@ router.post('/signup', (req, res) => {
 								})
 								.catch((err) => {
 									res.json({
-										status: 'FAILED',
-										message: 'An error occurred while saving user account!',
+										status: "FAILED",
+										message: "An error occurred while saving user account!",
 									});
 								});
 						})
 						.catch((err) => {
 							res.json({
-								status: 'FAILED',
-								message: 'An error occurred while hashing password!',
+								status: "FAILED",
+								message: "An error occurred while hashing password!",
 							});
 						});
 				}
@@ -116,8 +138,8 @@ router.post('/signup', (req, res) => {
 			.catch((err) => {
 				console.log(err);
 				res.json({
-					status: 'FAILED',
-					message: 'An error occurred while checking for existing user!',
+					status: "FAILED",
+					message: "An error occurred while checking for existing user!",
 				});
 			});
 	}
@@ -126,7 +148,7 @@ router.post('/signup', (req, res) => {
 //send verify email
 const sendVerificationEmail = ({ _id, email }, res) => {
 	//url to be used in the email
-	const currentUrl = 'https://secure-depths-90020.herokuapp.com/';
+	const currentUrl = "https://secure-depths-90020.herokuapp.com/";
 
 	const uniqueString = uuidv4() + _id;
 
@@ -134,11 +156,11 @@ const sendVerificationEmail = ({ _id, email }, res) => {
 	const mailOptions = {
 		from: process.env.AUTH_EMAIL,
 		to: email,
-		subject: 'Verify your Email',
+		subject: "Verify your Email",
 		html: `<p>Verify your email adress to complete the signup and login to your account.</p>
           <p><b>This link expires in 6 hours</b>.</p><p>Press <a href=${
-				currentUrl + 'user/verify/' + _id + '/' + uniqueString
-			}>here</a> to proceed.</p>`,
+						currentUrl + "user/verify/" + _id + "/" + uniqueString
+					}>here</a> to proceed.</p>`,
 	};
 
 	// hash the uniqueString
@@ -161,36 +183,36 @@ const sendVerificationEmail = ({ _id, email }, res) => {
 						.then(() => {
 							//email sent and verification record saved
 							res.json({
-								status: 'PENDING',
-								message: 'Email sent',
+								status: "PENDING",
+								message: "Email sent",
 							});
 						})
 						.catch((error) => {
 							console.log(error);
 							res.json({
-								status: 'FAILED',
-								message: 'An error occurred while sending the mail!',
+								status: "FAILED",
+								message: "An error occurred while sending the mail!",
 							});
 						});
 				})
 				.catch((error) => {
 					console.log(error);
 					res.json({
-						status: 'FAILED',
+						status: "FAILED",
 						message: "Couldn't save the verification data",
 					});
 				});
 		})
 		.catch(() => {
 			res.json({
-				status: 'FAILED',
-				message: 'An error has occurred while hashing the email data!',
+				status: "FAILED",
+				message: "An error has occurred while hashing the email data!",
 			});
 		});
 };
 
 //verify email
-router.get('/verify/:userId/:uniqueString', (req, res) => {
+router.get("/verify/:userId/:uniqueString", (req, res) => {
 	let { userId, uniqueString } = req.params;
 
 	UserVerification.find({ userId })
@@ -208,18 +230,18 @@ router.get('/verify/:userId/:uniqueString', (req, res) => {
 						.then((result) => {
 							User.deleteOne({ _id: userId })
 								.then(() => {
-									let message = 'Link has expired. Please sign up again.';
+									let message = "Link has expired. Please sign up again.";
 									res.redirect(`/user/verified/error=true&message=${message}`);
 								})
 								.catch((error) => {
 									console.log(error);
-									let message = 'Clearing User with expired uniqueString failed';
+									let message = "Clearing User with expired uniqueString failed";
 									res.redirect(`/user/verified/error=true&message=${message}`);
 								});
 						})
 						.catch((error) => {
 							console.log(error);
-							let message = 'An error occurred while clearing expired user verification record';
+							let message = "An error occurred while clearing expired user verification record";
 							res.redirect(`/user/verified/error=true&message=${message}`);
 						});
 				} else {
@@ -236,27 +258,27 @@ router.get('/verify/:userId/:uniqueString', (req, res) => {
 									.then(() => {
 										UserVerification.deleteOne({ userId })
 											.then(() => {
-												res.sendFile(path.join(__dirname, './../views/verified.html'));
+												res.sendFile(path.join(__dirname, "./../views/verified.html"));
 											})
 											.catch((error) => {
 												console.log(error);
-												let message = 'Invalid verification details passed. Check your Inbox.';
+												let message = "Invalid verification details passed. Check your Inbox.";
 												res.redirect(`/user/verified/error=true&message=${message}`);
 											});
 									})
 									.catch((error) => {
 										console.log(error);
-										let message = 'An Error occured while updating user record to show verified.';
+										let message = "An Error occured while updating user record to show verified.";
 										res.redirect(`/user/verified/error=true&message=${message}`);
 									});
 							} else {
 								//existing record but incorrect verification details passed.
-								let message = 'Invalid verification details passed. Check your Inbox.';
+								let message = "Invalid verification details passed. Check your Inbox.";
 								res.redirect(`/user/verified/error=true&message=${message}`);
 							}
 						})
 						.catch((error) => {
-							let message = 'An error occurred while comparing the unique strings.';
+							let message = "An error occurred while comparing the unique strings.";
 							res.redirect(`/user/verified/error=true&message=${message}`);
 						});
 				}
@@ -268,26 +290,26 @@ router.get('/verify/:userId/:uniqueString', (req, res) => {
 		})
 		.catch((error) => {
 			console.log(error);
-			let message = 'An error occurred while checking for existing user verification record';
+			let message = "An error occurred while checking for existing user verification record";
 			res.redirect(`/user/verified/error=true&message=${message}`);
 		});
 });
 
 //Verified page route
-router.get('/verified', (req, res) => {
-	res.sendFile(path.join(__dirname, './../views/verified.html'));
+router.get("/verified", (req, res) => {
+	res.sendFile(path.join(__dirname, "./../views/verified.html"));
 });
 
 // Signin
-router.post('/signin', (req, res) => {
+router.post("/signin", (req, res) => {
 	let { email, password } = req.body;
 	email = email.trim();
 	password = password.trim();
 
-	if (email == '' || password == '') {
+	if (email == "" || password == "") {
 		res.json({
-			status: 'FAILED',
-			message: 'Empty credentials supplied',
+			status: "FAILED",
+			message: "Empty credentials supplied",
 		});
 	} else {
 		// Check if user exist
@@ -300,8 +322,8 @@ router.post('/signin', (req, res) => {
 
 					if (!data[0].verified) {
 						res.json({
-							status: 'FAILED',
-							message: 'Email has not been verified yet! Please check your inbox.',
+							status: "FAILED",
+							message: "Email has not been verified yet! Please check your inbox.",
 						});
 					} else {
 						const hashedPassword = data[0].password;
@@ -311,38 +333,150 @@ router.post('/signin', (req, res) => {
 								if (result) {
 									// Password match
 									res.json({
-										status: 'SUCCESS',
-										message: 'Signin successful',
+										status: "SUCCESS",
+										message: "Signin successful",
 										data: data,
 									});
 								} else {
 									res.json({
-										status: 'FAILED',
-										message: 'Invalid password entered!',
+										status: "FAILED",
+										message: "Invalid password entered!",
 									});
 								}
 							})
 							.catch((err) => {
 								res.json({
-									status: 'FAILED',
-									message: 'An error occurred while comparing passwords',
+									status: "FAILED",
+									message: "An error occurred while comparing passwords",
 								});
 							});
 					}
 				} else {
 					res.json({
-						status: 'FAILED',
-						message: 'Invalid credentials entered!',
+						status: "FAILED",
+						message: "Invalid credentials entered!",
 					});
 				}
 			})
 			.catch((err) => {
 				res.json({
-					status: 'FAILED',
-					message: 'An error occurred while checking for existing user',
+					status: "FAILED",
+					message: "An error occurred while checking for existing user",
 				});
 			});
 	}
 });
+
+//password reset stuff
+router.post("/requestPasswordReset", (req, res) => {
+	const { email, redirectUrl } = req.body;
+
+	User.find({ email })
+		.then((data) => {
+			if (data.length) {
+				//user exists
+
+				//checking if user is verifies
+
+				if (!data[0].verified) {
+					res.json({
+						status: "FAILED",
+						message: "Email has not been verified yet",
+					});
+				} else {
+					//proceed with email reset password
+					sendResetEmail(data[0], redirectUrl, res);
+				}
+			} else {
+				res.json({
+					status: "FAILED",
+					message: "Account not found.",
+				});
+			}
+		})
+		.catch((error) => {
+			console.log(error);
+			res.json({
+				status: "FAILED",
+				message: "An error occurred while checking for existing user",
+			});
+		});
+});
+
+//send reset email
+const sendResetEmail = ({ _id, email }, redirectUrl, res) => {
+	const resetString = uuidv4 + _id;
+
+	//First clear all existing reset records
+	PasswordReset.deleteMany({ userId: _id })
+		.then((result) => {
+			//reset records deleted succesfully
+			//Now we send the email
+
+			//mail options
+			const mailOptions = {
+				from: process.env.AUTH_EMAIL,
+				to: email,
+				subject: "Password Reset",
+				html: `<p>We heared that you lost your password.</p> <p>Don't worry, use the link below to reset it.</p>
+          		<p><b>This link expires in 15 minutes</b>.</p><p>Press <a href=${redirectUrl + "/" + _id + "/" + resetString}>here</a> to proceed.</p>`,
+			};
+			//hash the reset string
+			const saltRounds = 10;
+			bcrypt
+				.hash(resetString, saltRounds)
+				.then((hashedResetString) => {
+					const newPasswordReset = new PasswordReset({
+						userId: _id,
+						resetString: hashedResetString,
+						createdAt: Date.now(),
+						expiresAt: Date.now() + 900000,
+					});
+
+					newPasswordReset
+						.save()
+						.then(() => {
+							transporter
+								.sendMail(mailOptions)
+								.then(() => {
+									//reset email sent and reset recrd has been saved
+									res.json({
+										status: "PENDING",
+										message: "Password reset email sent.",
+									});
+								})
+								.catch((error) => {
+									console.log(error);
+									res.json({
+										status: "FAILED",
+										message: "Reset email couldn't be send.",
+									});
+								});
+						})
+						.catch((error) => {
+							console.log(error);
+							res.json({
+								status: "FAILED",
+								message: "Couldn't save the new password.",
+							});
+						});
+				})
+				.catch((error) => {
+					console.log(error);
+					res.json({
+						status: "FAILED",
+						message: "Error while hashing the reset string.",
+					});
+				});
+		})
+		.catch((error) => {
+			//error while clearing existing records
+			console.log(error);
+			res.json({
+				status: "FAILED",
+				message: "Clearing existing user records failed",
+			});
+		});
+};
 
 module.exports = router;
